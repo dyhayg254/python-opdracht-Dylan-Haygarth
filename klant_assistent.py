ebe6077495d94c01593fbc7598c12b3e04acd341
Dylan Haygarth <dyhayg@gmail.com>
2026-05-05 15:13:55 +0200

    Iteratie 4: robuustheid
    voeg checks toe bij het opvragen van user input doormiddel van while loops

--- klant_assistent.py
+++ klant_assistent.py
@@ -23,9 +23,20 @@ Spelregels:
 - Werk toe naar een werkende terminal-applicatie
 """
 
+geldige_klanttypes = ["nieuw", "bestaand", "premium"]
+
 def verwerk_klanten():
     klanten = []
-    aantalKlanten = int(input("Hoeveel klanten wilt u invullen? "))
+
+    # blijf de vraag opnieuw stellen totdat de gebruiker een heel getal groter dan 0 invoert.
+    while True:
+        try:
+            aantalKlanten = int(input("Hoeveel klanten wilt u invullen? "))
+            if aantalKlanten > 0:
+                break
+            print("Het aantal moet groter zijn dan 0.")
+        except ValueError:
+            print("Voer een geldig geheel getal in.")
 
     for i in range(aantalKlanten):
         klant = verzamel_klant(i + 1)
@@ -70,10 +81,38 @@ def verzamel_klant(klantNummer):
     }
 
     print("klant nummer:", klantNummer)
+
     naam = str(input("Wat is de naam van deze klant? "))
-    leeftijd = int(input("Wat is de leeftijd van deze klant? "))
-    besteding = float(input("Wat is het bestedingsniveau van deze klant? "))
-    klanttype = str(input("Wat is het klanttype van deze klant? (nieuw/bestaand/premium) "))
+    # blijf de leeftijd vragen totdat de gebruiker een legitiem getal invoert
+    while True:
+        try:
+            leeftijd = int(input("Wat is de leeftijd van deze klant? "))
+            if leeftijd >= 0:
+                break
+            print("Leeftijd kan niet negatief zijn.")
+        except ValueError:
+            print("Voer een geldig geheel getal in.")    
+
+    # blijf het bestedingsniveau vragen totdat de gebruiker een legitiem getal invoert
+    while True:
+        try:
+            besteding = float(input("Wat is het bestedingsniveau van deze klant? "))
+            if besteding >= 0:
+                break
+            print("Besteding kan niet negatief zijn.")
+        except ValueError:
+            print("Voer een geldig bedrag in (bijvoorbeeld 100 of 99.95).")
+
+    # check of de klant een van de 3 geldige opties als klanttype invoert
+    while True:
+        klanttype = input(
+            "Wat is het klanttype van deze klant? (nieuw/bestaand/premium) "
+        ).lower()
+
+        if klanttype in geldige_klanttypes:
+            break
+        else:
+            print("Ongeldig klanttype. Kies uit: nieuw, bestaand of premium.")
 
     klant.update({
         "naam": naam,
